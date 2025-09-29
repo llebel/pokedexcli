8872c43c425faf2e750d5c380531cc5e460db2a4
llebel <loic.lebel@gmail.com>
2025-09-29 19:48:49 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/llebel/pokedexcli
+
+go 1.24.1
---
+++ repl_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  Hello, World!    ",
+			expected: []string{"hello,", "world!"},
+		},
+		// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(\"%s\") got size:%d expected:%d", c.input, len(actual), len(c.expected))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("cleanInput(\"%s\") got:%s expected:%s", c.input, word, expectedWord)
+			}
+		}
+	}
+}
