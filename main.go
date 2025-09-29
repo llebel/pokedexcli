4d4d26f6226fa0c146f4a917b9695f6b6660eeb0
llebel <loic.lebel@gmail.com>
2025-09-29 20:03:39 +0000

    Start implemeting repl

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -10,6 +12,14 @@ func cleanInput(text string) []string {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(cleanInput("  hello  world  "))
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		fmt.Println("Your command was: " + cleanedInput[0])
+	}
+
 }
