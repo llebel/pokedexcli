a949606e74f77b048171cb1bd35aa18facf68401
llebel <loic.lebel@gmail.com>
2025-09-29 20:40:44 +0000

    Refactor REPL into separate file and commands

---
+++ command_help.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, command := range getCommands() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
