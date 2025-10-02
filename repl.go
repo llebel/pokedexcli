bb332a4e3c98a06e49a9db7c68cf7f8d00f35048
llebel <loic.lebel@gmail.com>
2025-10-02 15:25:40 +0000

    Refactor cache into client

--- repl.go
+++ repl.go
@@ -7,12 +7,10 @@ import (
 	"strings"
 
 	"github.com/llebel/pokedexcli/internal/pokeapi"
-	"github.com/llebel/pokedexcli/internal/pokecache"
 )
 
 type cliConfig struct {
 	pokeapiClient    pokeapi.Client
-	pokeCache        *pokecache.Cache
 	nextLocationsURL *string
 	prevLocationsURL *string
 }
