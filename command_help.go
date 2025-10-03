f4a4cc49fefc1c95c0ac5df94c39348c342c7700
llebel <loic.lebel@gmail.com>
2025-10-03 09:36:55 +0000

    Cosmetics: uniformize cliConfig var name

--- command_help.go
+++ command_help.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func commandHelp(context *cliConfig, args ...string) error {
+func commandHelp(cfg *cliConfig, args ...string) error {
 	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
 	for _, command := range getCommands() {
 		fmt.Printf("%s: %s\n", command.name, command.description)
